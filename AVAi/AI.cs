169f6a9e8983cc69d33a32aaabedf0d4223b5204
Saša Barišić <cartman300@net.hr>
2016-03-18 12:15:29 +0100

    Project files

---
+++ AVAi/AI.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AVAi {
+	public class AI {
+		public void Process(string In) {
+			Console.WriteLine(In);
+		}
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AVAi;
+
+namespace Test {
+	class Program {
+		static string Prompt() {
+			string In = "";
+			while (In.Length == 0) {
+				Console.Write("$ ");
+				In = Console.ReadLine().Trim();
+			}
+			return In;
+		}
+
+		static void Main(string[] args) {
+			Console.Title = "AVA AI Test";
+
+			AI Ava = new AI();
+
+			while (true)
+				Ava.Process(Prompt());
+
+			/*Console.WriteLine("Done!");
+			Console.ReadLine();*/
+		}
+	}
+}
